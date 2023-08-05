cf3728072620023a3fccdcbbfd60b4232ba0e9ea
Daniel Gray <danielscgray@gmail.com>
2023-08-05 14:35:52 -0700

    final number assignment

--- Flask/Practice3_Great_Number_Game/server.py
+++ Flask/Practice3_Great_Number_Game/server.py
@@ -1,4 +1,4 @@
-from random import random
+import random
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = 'ea3fdb7f1bcf66419ab5eb540eecbc8dc82941cf7e073d49b4def6928d801f25'
@@ -6,8 +6,18 @@ app.secret_key = 'ea3fdb7f1bcf66419ab5eb540eecbc8dc82941cf7e073d49b4def6928d801f
 @app.route('/')
 def home():
     if 'winning_number' not in session:
-        session['winning_number'] = 
+        session['winning_number'] = random.randint(1, 100)
     return render_template('index.html')
+
+@app.route('/guess', methods=['POST'])
+def guess():
+    session['user_guess'] = int(request.form['user_guess'])
+    return redirect('/')
+
+@app.route('/win', methods=['POST'])
+def reset():
+    session.clear()
+    return redirect('/')
 
 if __name__=="__main__":
     app.run(debug=True)
