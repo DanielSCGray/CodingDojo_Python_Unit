a4c0c8693c84fd4e22fd575614e2f558f040b9cd
Daniel Gray <danielscgray@gmail.com>
2023-08-11 15:19:28 -0700

    templates+wireframe

--- Flask_and_MySQL/Core3_Login_and_Registration/flask_app/controllers/users.py
+++ Flask_and_MySQL/Core3_Login_and_Registration/flask_app/controllers/users.py
@@ -2,3 +2,22 @@ from flask_app import app
 from flask_app.models import user
 from flask import redirect, render_template, request, session
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.post('/register')
+def register():
+    if not user.User.validate_registration(request.form):
+        return redirect('/')
+    return redirect('/welcome')
+
+@app.post('/log_in')
+def log_in():
+    if not user.User.validate_login(request.form):
+        return redirect('/')
+    return redirect('/welcome')
+
+@app.route('/welcome')
+def welcome_page():
+    return render_template('welcome.html')
--- Flask_and_MySQL/Core3_Login_and_Registration/flask_app/models/user.py
+++ Flask_and_MySQL/Core3_Login_and_Registration/flask_app/models/user.py
@@ -9,3 +9,17 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 class User:
     def __init__(self, data):
         pass
+
+    @staticmethod
+    def validate_registration():
+        pass
+
+    @staticmethod
+    def validate_login():
+        pass
+
+    @classmethod
+    def create(cls, form_data):
+        query = '''INSERT INTO users (first_name, last_name, email, password)
+        VALUES ( %(first_name)s,  %(last_name)s, %(email)s, %(password)s,)'''
+        pass
