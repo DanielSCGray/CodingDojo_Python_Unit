1101382369b90fad0651f1a01bde716581a21ea1
Daniel Gray <danielscgray@gmail.com>
2023-08-13 16:36:01 -0700

    full functionality + bcrypt pws

--- Flask_and_MySQL/Core3_Login_and_Registration/flask_app/controllers/users.py
+++ Flask_and_MySQL/Core3_Login_and_Registration/flask_app/controllers/users.py
@@ -1,6 +1,9 @@
 from flask_app import app
 from flask_app.models import user
-from flask import redirect, render_template, request, session
+from flask import redirect, render_template, request, session, flash
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def home():
@@ -10,14 +13,35 @@ def home():
 def register():
     if not user.User.validate_registration(request.form):
         return redirect('/')
+    password_hash = bcrypt.generate_password_hash(request.form['password'])
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email'],
+        'password': password_hash
+    }
+    user_id = user.User.create(data)
+    session['user_id'] = user_id
     return redirect('/welcome')
 
 @app.post('/log_in')
 def log_in():
-    if not user.User.validate_login(request.form):
+    data = { 'email': request.form['email'] }
+    current_user = user.User.get_by_email(data)
+    if not current_user:
+        flash('Invalid email/password')
         return redirect('/')
+    if not bcrypt.check_password_hash(current_user.password, request.form['password']):
+        flash('Invalid email/password')
+        return redirect('/')
+    session['user_id'] = current_user.id
     return redirect('/welcome')
 
 @app.route('/welcome')
 def welcome_page():
     return render_template('welcome.html')
+
+@app.post('/log_out')
+def log_out():
+    session.clear()
+    return redirect('/')
